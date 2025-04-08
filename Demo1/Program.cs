30999726cdb65e8257e696d98ef7731499ce0ed3
CoderKinley <penjorkinley313@gmail.com>
2025-04-08 18:11:57 +0530

    added the development branch

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -8,6 +8,7 @@ namespace Demo1
         [STAThread]
         static void Main()
         {
+            // normal commit
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
